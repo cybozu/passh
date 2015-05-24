25fd2ea0cb8cf6af40966cf3e5a099c597af57fb
ymmt2005 <ymmt2005@gmail.com>
2015-05-25 07:47:52 +0900

    version 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'passh',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
