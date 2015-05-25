07fdaeb691ac9db7f6d4a5e3c358a49994a92130
ymmt2005 <ymmt2005@gmail.com>
2015-05-25 09:08:28 +0900

    add metadata to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,41 @@ setup(
             'passh=passh:main',
         ],
     },
+
+    # https://docs.python.org/3.4/distutils/setupscript.html#meta-data
+    author = 'Yamamoto, Hirotaka',
+    author_email = 'ymmt2005@gmail.com',
+    maintainer = 'Yamamoto, Hirotaka',
+    maintainer_email = 'ymmt2005@gmail.com',
+    url = 'https://github.com/cybozu/passh',
+    description = 'Python3 asyncio library to run SSH in parallel',
+    long_description = '''passh (Parallel Asynchronous SSH) is a Python3 library to run SSH processes in parallel.
+
+As passh depends on `asyncio <https://docs.python.org/3/library/asyncio.html>`_,  Python 3.4 or newer is required.
+
+Features:
+
+* ``PAssh`` class to run SSH in parallel.
+  
+  * SSH outputs are forwarded to local stdout/stderr.
+  * Every line is to be prefixed by the remote hostname.
+  * Instead of forwarding, SSH outputs can be collected in memory for later use.
+
+* Non-asyncio apps can use passh as well as asyncio apps.
+* A file can be given as inputs for all SSH processes.
+* Limit on the number of simultaneous SSH processes.
+* Built-in command-line interface.
+''',
+    license = 'MIT',
+    keywords = 'asyncio ssh',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking',
+    ],
 )
