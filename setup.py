49f697d37a910155d6364044e3dde60045e3d6e5
ymmt2005 <ymmt2005@gmail.com>
2016-01-29 16:42:32 +0900

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'passh',
-    version = '1.0.1',
+    version = '1.0.2',
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
